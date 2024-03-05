bb15f99e0a5266a839265f70d5115b01704df589
rupeshshandilya <rupeshkshandilya@gmail.com>
2024-03-06 03:51:20 +0530

    feat(ProfileDropDown.tsx): add support for next-auth session data refactor(ProfileDropDown.tsx): use next-auth session data for user info if available fix(ProfileDropDown.tsx): use next-auth signOut function in logoutHandler to ensure proper session termination feat(ProfileDropDown.tsx): add registerUser function to register user data from next-auth session

--- client/user-ui/src/components/ProfileDropDown.tsx
+++ client/user-ui/src/components/ProfileDropDown.tsx
@@ -12,17 +12,26 @@ import { CgProfile } from "react-icons/cg";
 import useUser from "@/hooks/useUser";
 import Cookies from "js-cookie";
 import toast from "react-hot-toast";
+import { signOut, useSession } from "next-auth/react";
+import { register } from "module";
+import { registerUser } from "@/actions/register-user";
 
 const ProfileDropDown = () => {
   const [signedIn, setSignedIn] = useState(false);
   const [open, setOpen] = useState(false);
   const { user, loading } = useUser();
+  const { data } = useSession()
+  
 
   useEffect(() => {
     if (!loading) {
       setSignedIn(!!user);
     }
-  }, [loading, user, open]);
+    if(data?.user){
+      setSignedIn(true)
+      addUser(data?.user);
+    }
+  }, [loading, user, open, data]);
 
   const logoutHandler = () => {
     Cookies.remove("access_token");
@@ -30,6 +39,10 @@ const ProfileDropDown = () => {
     toast.success("Logout Successfully");
     window.location.reload();
   };
+
+  const addUser = async (user: any) => {
+    await registerUser(user);
+  }
 
   return (
     <div className="flex items-center gap-4">
@@ -39,20 +52,20 @@ const ProfileDropDown = () => {
             <Avatar
               as="button"
               className="transition-transform"
-              src={user?.avatar?.url}
+              src={data?.user ? data.user.image : user.image}
             />
           </DropdownTrigger>
           <DropdownMenu aria-label="Profile Actions" variant="flat">
             <DropdownItem key="profile" className="h-14 gap-2">
               <p className="font-semibold">Signed in as</p>
-              <p className="font-semibold">{user.email}</p>
+              <p className="font-semibold">{data?.user ? data?.user.email : user.email}</p>
             </DropdownItem>
             <DropdownItem key="settings">Profile</DropdownItem>
             <DropdownItem key="all_orders">Orders</DropdownItem>
             <DropdownItem key="seller_settings">
               Apply for seller Account
             </DropdownItem>
-            <DropdownItem key="logout" color="danger" onClick={logoutHandler}>
+            <DropdownItem key="logout" color="danger" onClick={() => signOut() || logoutHandler }>
               Logout
             </DropdownItem>
           </DropdownMenu>
