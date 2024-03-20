c67f6bfd69416bff91fb91cd80db336fa7c67439
rupeshshandilya <rupeshkshandilya@gmail.com>
2024-03-20 20:03:04 +0530

    refactor(Prisma.service.ts): convert PrismaService to singleton pattern to prevent multiple instances feat(Prisma.service.ts): add logging for queries to improve debugging docs(Prisma.service.ts): add comments about PrismaClient usage in development environment

--- server/apps/users/prisma/Prisma.service.ts
+++ server/apps/users/prisma/Prisma.service.ts
@@ -1,9 +1,38 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { OnModuleInit } from '@nestjs/common'
+import { PrismaClient } from '@prisma/client'
 
-@Injectable()
+// PrismaClient is attached to the `global` object in development to prevent
+// exhausting your database connection limit.
+//
+// Learn more:
+// https://pris.ly/d/help/next-js-best-practices
+
+const globalForPrisma = global as unknown as { prisma: PrismaClient }
+
 export class PrismaService extends PrismaClient implements OnModuleInit {
+
+  private static instance: PrismaService
+
+  constructor(){
+    super(
+      {
+        log: ['query']
+      }
+    )
+    if (!PrismaService.instance) {
+      PrismaService.instance = this;
+    }
+    return PrismaService.instance;
+  }
+
   async onModuleInit() {
-    await this.$connect();
+    await this.$connect
+  }
+
+  public static getInstance(): PrismaService {
+    if (!PrismaService.instance) {
+      PrismaService.instance = new PrismaService();
+    }
+    return PrismaService.instance;
   }
 }
