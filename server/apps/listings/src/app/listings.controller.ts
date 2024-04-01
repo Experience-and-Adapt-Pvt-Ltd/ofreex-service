e9daa7e795e4fd1b9e10abee61dd0f6cdcb96ffd
rupeshshandilya <rupeshkshandilya@gmail.com>
2024-04-01 20:55:29 +0530

    removed uncessary file

--- server/apps/listings/src/app/listings.controller.ts
+++
@@ -1,8 +0,0 @@
-import { Controller, Get } from '@nestjs/common';
-
-import { ListingsService } from './listings.service';
-
-@Controller()
-export class ListingsController {
-  constructor(private readonly appService: ListingsService) { }
-}
