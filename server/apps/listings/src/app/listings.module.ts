f5d78a616e2a2002bfde50c418e74c307b2c8ac3
rupeshshandilya <rupeshkshandilya@gmail.com>
2024-04-02 18:48:22 +0530

    feat(listings.module.ts): add HttpModule to imports to enable HTTP requests feat(listings.module.ts): add PrismaClient to providers to enable database operations

--- server/apps/listings/src/app/listings.module.ts
+++ server/apps/listings/src/app/listings.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 
 import { ListingsController } from './listings.controller';
 import { ListingsService } from './listings.service';
+import { HttpModule } from '@nestjs/axios';
+import { PrismaClient, Listing as PrismaListing } from '../../node_modules/.prisma/client';
 
 @Module({
-  imports: [],
+  imports: [HttpModule],
   controllers: [ListingsController],
-  providers: [ListingsService],
+  providers: [ListingsService, PrismaClient],
 })
 export class AppModule { }
