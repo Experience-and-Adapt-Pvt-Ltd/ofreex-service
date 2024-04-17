59098031c52df44505900dae0423e0915fb5b0eb
rupeshshandilya <rupeshkshandilya@gmail.com>
2024-04-17 12:13:30 +0530

    to add open and close registration modal

--- client/ofreex-frontend/app/hooks/useSellerLoginModal.ts
+++ client/ofreex-frontend/app/hooks/useSellerLoginModal.ts
@@ -1,16 +1,17 @@
 import { create } from 'zustand';
 
-interface SellerActivationModalStore {
+interface SellerLoginModal {
   isOpen: boolean;
   onOpen: () => void;
   onClose: () => void;
 }
 
-const useSellerActivationModal = create<SellerActivationModalStore>((set) => ({
+const useSellerLoginModal = create<SellerLoginModal>((set) => ({
   isOpen: false,
   onOpen: () => set({ isOpen: true }),
   onClose: () => set({ isOpen: false })
 }));
 
 
-export default useSellerActivationModal;
+
+export default useSellerLoginModal;
