99967ad9020cff0ded1e9d02cd825e3b55d583d2
rupeshshandilya <rupeshkshandilya@gmail.com>
2024-04-17 12:20:14 +0530

    added carosuel

--- client/ofreex-frontend/app/components/listings/ListingHead.tsx
+++ client/ofreex-frontend/app/components/listings/ListingHead.tsx
@@ -1,6 +1,9 @@
-'use client';
-
+'use state'
+import React, { useState } from 'react';
 import Image from "next/image";
+import { Carousel } from "@material-tailwind/react";
+import Swipe from "react-easy-swipe";
+import { AiOutlineLeft, AiOutlineRight } from "react-icons/ai";
 
 import useCountries from "@/app/hooks/useCountries";
 import { SafeUser } from "@/app/types";
@@ -23,43 +26,75 @@ const ListingHead: React.FC<ListingHeadProps> = ({
   id,
   currentUser
 }) => {
-  // const { getByValue } = useCountries();
+  const [currentSlide, setCurrentSlide] = useState(0);
 
-  // const location = getByValue(locationValue);
-  console.log("Title of listing : " + title);
+  const handleNextSlide = () => {
+    let newSlide = currentSlide === imageUrls.length - 1 ? 0 : currentSlide + 1;
+    setCurrentSlide(newSlide);
+  };
+
+  const handlePrevSlide = () => {
+    let newSlide = currentSlide === 0 ? imageUrls.length - 1 : currentSlide - 1;
+    setCurrentSlide(newSlide);
+  };
+
   return (
     <>
-      <Heading
-        title={title}
-      // subtitle={`${location?.region}, ${location?.label}`}
+    <Heading 
+    title={title}
+    />
+
+    <div className="relative">
+      <AiOutlineLeft
+        onClick={handlePrevSlide}
+        className="absolute left-0 m-auto text-5xl inset-y-1/2 cursor-pointer text-gray-400 z-20"
       />
-      <div className="
-          w-full
-          h-[60vh]
-          overflow-hidden 
-          rounded-xl
-          relative
-        "
-      >
-        <Image
-          src={"/images/ofreex-logo.png"}
-          fill
-          className="object-cover w-full"
-          alt="Image"
-        />
-        <div
-          className="
-            absolute
-            top-5
-            right-5
-          "
+      <div className="w-full h-[50vh] flex overflow-hidden relative m-auto">
+        <Swipe
+          onSwipeLeft={handleNextSlide}
+          onSwipeRight={handlePrevSlide}
+          className="relative z-10 w-full h-full"
         >
-          <HeartButton
-            listingId={id}
-            currentUser={currentUser}
-          />
-        </div>
+          {imageUrls.map((image, index) => {
+            if (index === currentSlide) {
+              console.log(imageUrls);
+
+              return (
+                <Image
+                  key={image}
+                  fill
+                  style={{ objectFit: 'cover' }}
+                  src={image}
+                  alt={`Uploaded image ${index + 1}`}
+                />
+              );
+            }
+          })}
+        </Swipe>
       </div>
+      <AiOutlineRight
+        onClick={handleNextSlide}
+        className="absolute right-0 m-auto text-5xl inset-y-1/2 cursor-pointer text-gray-400 z-20"
+      />
+
+      <div className="relative flex justify-center p-2">
+        {imageUrls.map((_, index) => {
+          return (
+            <div
+              className={
+                index === currentSlide
+                  ? "h-4 w-4 bg-gray-700 rounded-full mx-2 mb-2 cursor-pointer"
+                  : "h-4 w-4 bg-gray-300 rounded-full mx-2 mb-2 cursor-pointer"
+              }
+              key={index}
+              onClick={() => {
+                setCurrentSlide(index);
+              }}
+            />
+          );
+        })}
+      </div>
+    </div>
     </>
   );
 }
