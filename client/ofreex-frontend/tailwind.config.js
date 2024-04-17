c635029486c96069416cef7daae40c01d0cc6f10
rupeshshandilya <rupeshkshandilya@gmail.com>
2024-04-17 11:57:05 +0530

    init

--- client/ofreex-frontend/tailwind.config.js
+++ client/ofreex-frontend/tailwind.config.js
@@ -4,10 +4,10 @@ module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
