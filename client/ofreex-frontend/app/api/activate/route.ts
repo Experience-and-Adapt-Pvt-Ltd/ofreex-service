20d34d3ebee817447e5279f45485f2ec76f74a7a
rupeshshandilya <rupeshkshandilya@gmail.com>
2024-04-30 18:14:53 +0530

    removed log

--- client/ofreex-frontend/app/api/activate/route.ts
+++ client/ofreex-frontend/app/api/activate/route.ts
@@ -4,17 +4,10 @@ import bcrypt from "bcrypt";
 import prisma from "@/app/libs/prismadb";
 import axios from "axios";
 
-export async function POST(
-  request: Request,
-) {
+export async function POST(request: Request) {
   const body = await request.json();
-  const {
-    activationToken,
-    activationCode
-  } = body;
-  console.log("asdfasfasdfasdfasfasdfadfasfafa");
-  const { data } = await axios.post(
-    `http://localhost:4001/graphql`, {
+  const { activationToken, activationCode } = body;
+  const { data } = await axios.post(`http://localhost:4001/graphql`, {
     query: `mutation activateUser {
   activateUser(activationDto:{
     activationToken: "${activationToken}",
@@ -33,11 +26,8 @@ export async function POST(
 
   }
 }
-
-`
-  }
-  )
-  console.log("hererererererererr");
+`,
+  });
 
   // const { id,
   //     name,
