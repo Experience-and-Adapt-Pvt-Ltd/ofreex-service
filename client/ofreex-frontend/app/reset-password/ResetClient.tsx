e803783bde12de08f69f971a027e0da7bad95ed4
rupeshshandilya <rupeshkshandilya@gmail.com>
2024-05-07 15:41:10 +0530

    clean code

--- client/ofreex-frontend/app/reset-password/ResetClient.tsx
+++ client/ofreex-frontend/app/reset-password/ResetClient.tsx
@@ -1,18 +1,7 @@
 'use client';
 
-import { toast } from "react-hot-toast";
-import axios from "axios";
-import { useCallback, useState } from "react";
-import { useRouter, useSearchParams } from "next/navigation";
-
-import { SafeCategory, SafeListing, SafeUser } from "@/app/types";
-
-import Heading from "@/app/components/Heading";
+import { useSearchParams } from "next/navigation";
 import Container from "@/app/components/Container";
-import ListingCard from "@/app/components/listings/ListingCard";
-import useEditModal from "../hooks/useEditModal";
-import EditModal from "../components/modals/EditModal";
-import { useEdit } from "../hooks/useEdit";
 import ResetModal from "../components/modals/ResetModal";
 import useResetModal from "../hooks/useResetModal";
 
@@ -25,7 +14,6 @@ const ResetClient: React.FC<ResetClientProps> = (
   const resetModal = useResetModal();
   resetModal.isOpen = true;
   const searchParams = useSearchParams();
-  console.log(searchParams?.get('verify'));
   return (
     <Container>
       <ResetModal verify={searchParams?.get('verify')} />
