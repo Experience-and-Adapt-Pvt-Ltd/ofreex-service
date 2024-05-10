3d686a600a405b155b62c6e9b6d0cfc4bfec8531
rupeshshandilya <rupeshkshandilya@gmail.com>
2024-05-10 15:56:04 +0530

    seller form completed

--- client/ofreex-frontend/app/sellerproductform/page.tsx
+++ client/ofreex-frontend/app/sellerproductform/page.tsx
@@ -1,21 +1,23 @@
-import React from 'react'
-import getCurrentSeller from '../actions/getCurrentSeller';
-import EmptyState from '../components/EmptyState';
-import ProductForm from './productForm';
+import React from "react";
+import getCurrentSeller from "../actions/getCurrentSeller";
+import EmptyState from "../components/EmptyState";
+import ProductForm from "./productForm";
+import getCategories from "../actions/getCategories";
 
-const productListingPage = () => {
-    const currentSeller = getCurrentSeller();
-    if(!currentSeller){
-        return <EmptyState
-      title="Unauthorized"
-      subtitle="Please login"
-    />
-    }
+const productListingPage =  async () => {
+  const currentSeller = await getCurrentSeller();
+  const categories = await getCategories();
+  
+
+  if (!currentSeller) {
+    return <EmptyState title="Unauthorized" subtitle="Please login" />;
+  }
   return (
     <>
-    <ProductForm /> 
+      <ProductForm 
+      categories = {categories}/>
     </>
-  )
-}
+  );
+};
 
 export default productListingPage;
--- client/ofreex-frontend/app/sellerproductform/productForm.tsx
+++ client/ofreex-frontend/app/sellerproductform/productForm.tsx
@@ -1,6 +1,91 @@
-import { PhotoIcon, UserCircleIcon } from "@heroicons/react/24/solid";
+"use client";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import toast from "react-hot-toast";
+import Input from "../components/inputs/Input";
+import ImageUpload from "../components/inputs/ImageUpload";
+import { SafeCategory } from "../types";
+import { useEdit } from "../hooks/useEdit";
+import Image from "next/image";
 
-const ProductForm = () => {
+interface ProductListModal {
+  categories: SafeCategory;
+}
+
+const ProductForm: React.FC<ProductListModal> = ({ categories }) => {
+  const router = useRouter();
+
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    watch,
+    formState: { errors },
+    reset,
+  } = useForm<FieldValues>({
+    defaultValues: {
+      category: "",
+      imageSrc: "",
+      price: "",
+      title: "",
+      description: "",
+      discount: "",
+      delivery: "",
+      quantity: "",
+      customCategory: "",
+    },
+  });
+
+  const [images, setImages] = useState<string[]>([]);
+  const category = watch("category");
+  const editHook = useEdit();
+  const showCustomCategory = category === "others";
+
+  const setCustomValue = (id: string, value: any) => {
+    setValue(id, value, {
+      shouldDirty: true,
+      shouldTouch: true,
+      shouldValidate: true,
+    });
+  };
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    const formData = {
+      ...data,
+      discount: parseFloat(data.discount),
+      images: images,
+      category: category === "others" ? "others" : data.category,
+      subCategory: category === "others" ? data.customCategory : undefined,
+    };
+
+    axios
+      .post("/api/listings", formData)
+      .then(() => {
+        toast.success("Product Listed!");
+        reset();
+        setImages([]);
+        // router.push("/properties");
+      })
+      .catch((error) => {
+        if (error.response) {
+          // Request made and server responded
+          console.log(error.response.data);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+          toast.error(`Error: ${error.response.data.message}`);
+        } else if (error.request) {
+          // The request was made but no response was received
+          console.log(error.request);
+          toast.error("No response from server");
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          console.log("Error", error.message);
+          toast.error("Error sending data");
+        }
+      });
+  };
   return (
     <div className="container mx-auto p-4">
       <div className="bg-white p-6 rounded shadow">
@@ -9,15 +94,53 @@ const ProductForm = () => {
         </h2>
         <form>
           <div className="space-y-4">
-            {/* <!-- Username --> */}
+            {/* Category */}
+            <div>
+              <label className="text-gray-900 font-medium block mt-4 mb-2">
+                Category
+              </label>
+              <select
+                id="category"
+                {...register("category")}
+                onChange={(e) => setCustomValue("category", e.target.value)}
+                className="form-select w-full px-3 py-2 border rounded shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500"
+              >
+                {categories.map((item, index) => (
+                  <option key={index} value={item.value}>
+                    {item.label}
+                  </option>
+                ))}
+              </select>
+              {showCustomCategory && (
+                <>
+                  <label className="text-gray-900 font-medium block mt-4 mb-2">
+                    Enter Category
+                  </label>
+                  <Input
+                    label="Custom Category"
+                    id="customCategory"
+                    type="text"
+                    className="form-Input w-full px-3 py-2 border rounded shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500"
+                    placeholder="Specify category"
+                    register={register}
+                    required
+                  />
+                </>
+              )}
+            </div>
+
+            {/* <!-- Product Name --> */}
             <div>
               <label className="text-gray-900 font-medium block mb-2">
                 Product Name
               </label>
-              <input
+              <Input
+                id="title"
                 type="text"
                 placeholder="ex: Phone"
-                className="form-input w-full px-3 py-2 border rounded shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500"
+                className="form-Input w-full px-3 py-2 border rounded shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500"
+                register={register}
+                required
               />
             </div>
 
@@ -26,11 +149,77 @@ const ProductForm = () => {
               <label className="text-gray-900 font-medium block mb-2">
                 Details of Product
               </label>
-              <textarea
+              {/* <textarea
                 rows={3}
                 placeholder="Write a few sentences about yourself."
                 className="form-textarea w-full px-3 py-2 border rounded shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500"
-              ></textarea>
+              ></textarea> */}
+              <Input
+                id="description"
+                type="text"
+                register={register}
+                placeholder="Enter data of product"
+                className="form-Input w-full px-3 py-2 border rounded shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500"
+                required
+              />
+            </div>
+
+            {/* Price */}
+            <div>
+              <label className="text-gray-900 font-medium block mt-4 mb-2">
+                Price
+              </label>
+              <Input
+                id="price"
+                type="number"
+                placeholder="ex: ₹500"
+                className="form-Input w-full px-3 py-2 border rounded shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500"
+                register={register}
+              />
+            </div>
+
+            {/* Quantity */}
+            <div>
+              <label className="text-gray-900 font-medium block mt-4 mb-2">
+                Quantity
+              </label>
+              <Input
+                id="quantity"
+                type="number"
+                placeholder="ex: 10"
+                className="form-Input w-full px-3 py-2 border rounded shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500"
+                register={register}
+              />
+            </div>
+
+            {/* Discount */}
+            <div>
+              <label className="text-gray-900 font-medium block mt-4 mb-2">
+                Discount
+              </label>
+              <Input
+                id="discount"
+                type="number"
+                placeholder="ex: 10%"
+                className="form-Input w-full px-3 py-2 border rounded shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500"
+                register={register}
+              />
+            </div>
+
+            {/* Delivery */}
+            <div>
+              <label className="text-gray-900 font-medium block mt-4 mb-2">
+                Delivery
+              </label>
+              <select
+                id="delivery"
+                {...register("delivery")}
+                className="form-select w-full px-3 py-2 border rounded shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500"
+                onChange={(e) => setCustomValue("delivery", e.target.value)}
+              >
+                <option value="own">Self Delivery</option>
+                <option value="ofreex">Ofreex Delivery</option>
+              </select>
             </div>
 
             {/* <!-- Photo Upload --> */}
@@ -38,89 +227,35 @@ const ProductForm = () => {
               <label className="text-gray-900 font-medium block mb-2">
                 Photo
               </label>
-              <div className="flex items-center justify-center w-full">
-                <label className="flex flex-col w-full h-32 border-4 border-dashed hover:bg-gray-100 hover:border-gray-300">
-                  <div className="flex flex-col items-center justify-center pt-7">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="w-12 h-12 text-gray-400 group-hover:text-gray-600"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="M3 7v4a1 1 0 001 1h3m10 0h3a1 1 0 001-1V7m-7 12V10m0 12v-5m0 5H9m3 0h3"
-                      />
-                    </svg>
-                    <p className="text-sm text-gray-400 group-hover:text-gray-600 pt-1 tracking-wider">
-                      Upload a file
-                    </p>
-                  </div>
-                  <input type="file" className="opacity-0" />
-                </label>
+              <div className="flex items-center justify-center w-full rounded-sm border">
+                <ImageUpload
+                  onChange={(value) => setImages(value)}
+                  value={images}
+                />
               </div>
             </div>
+
+            {/* Image Display Section */}
+            <div>
+              <label className="text-gray-900 font-medium block mb-2">
+                Uploaded Images
+              </label>
+              {images.length > 0 && (
+                <div className="grid grid-cols-3 gap-4 mt-4">
+                  {images.map((image, index) => (
+                    <div key={index} className="w-full h-32 relative">
+                      <Image
+                        src={image}
+                        alt={`Uploaded Image ${index}`}
+                        layout="fill"
+                        objectFit="cover"
+                      />
+                    </div>
+                  ))}
+                </div>
+              )}
+            </div>
           </div>
-
-          {/* Price */}
-          <div>
-            <label className="text-gray-900 font-medium block mt-4 mb-2">
-              Price
-            </label>
-            <input
-              type="text"
-              placeholder="ex: Phone"
-              className="form-input w-full px-3 py-2 border rounded shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500"
-            />
-          </div>
-
-          {/* Discount */}
-          <div>
-            <label className="text-gray-900 font-medium block mt-4 mb-2">
-              Discount
-            </label>
-            <input
-              type="text"
-              placeholder="ex: Phone"
-              className="form-input w-full px-3 py-2 border rounded shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500"
-            />
-          </div>
-
-          {/* Quantity */}
-          <div>
-            <label className="text-gray-900 font-medium block mt-4 mb-2">
-              Quantity
-            </label>
-            <input
-              type="text"
-              placeholder="ex: Phone"
-              className="form-input w-full px-3 py-2 border rounded shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500"
-            />
-          </div>
-
-          {/* Category */}
-          <div>
-          <label className="text-gray-900 font-medium block mt-4 mb-2">Category</label>
-          <select className="form-select w-full px-3 py-2 border rounded shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500">
-            <option value="">Select Category</option>
-            <option value="usa">Sports</option>
-            <option value="canada">Furniture</option>
-            <option value="uk">Others</option>
-          </select>
-        </div>
-
-        {/* Delivery */}
-        <div>
-          <label className="text-gray-900 font-medium block mt-4 mb-2">Delivery</label>
-          <select className="form-select w-full px-3 py-2 border rounded shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500">
-            <option value="">Select Delivery</option>
-            <option value="usa">Self Delivery</option>
-            <option value="canada">Ofreex Delivery</option>
-          </select>
-        </div>
 
           {/* <!-- Buttons --> */}
           <div className="flex justify-end space-x-4 mt-4">
@@ -133,6 +268,7 @@ const ProductForm = () => {
             <button
               type="submit"
               className="bg-blue-600 text-white rounded px-4 py-2 hover:bg-blue-700"
+              onClick={handleSubmit(onSubmit)}
             >
               Save
             </button>
