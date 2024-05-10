c086dcfc92a390fd9b4c1d29aab3aedc54c287e0
rupeshshandilya <rupeshkshandilya@gmail.com>
2024-05-10 14:20:39 +0530

    increased size of search bar and center it, responsive to navbar

--- client/ofreex-frontend/app/components/navbar/Logo.tsx
+++ client/ofreex-frontend/app/components/navbar/Logo.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import Image from "next/image";
 import { useRouter } from "next/navigation";
@@ -6,16 +6,16 @@ import { useRouter } from "next/navigation";
 const Logo = () => {
   const router = useRouter();
 
-  return ( 
+  return (
     <Image
-      onClick={() => router.push('/')}
-      className="relative mt-5 left-0 mr-5 cursor-pointer w-[200px] h-[200px]" 
-      src="/images/logo.png" 
-      height="100" 
-      width="100" 
-      alt="Logo" 
+      onClick={() => router.push("/")}
+      className="relative mt-4 left-0 cursor-pointer w-[150px] sm:w-[180px] md:w-[240px] h-[150px] sm:h-[120px] md:h-[240px]"
+      src="/images/logo.png"
+      height="100"
+      width="100"
+      alt="Logo"
     />
-   );
-}
- 
+  );
+};
+
 export default Logo;
