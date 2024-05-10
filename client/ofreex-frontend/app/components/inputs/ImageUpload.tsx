184c0d164d0f7acd38f200499ce660ac294636a7
rupeshshandilya <rupeshkshandilya@gmail.com>
2024-05-10 15:51:43 +0530

    changed svg and image url

--- client/ofreex-frontend/app/components/inputs/ImageUpload.tsx
+++ client/ofreex-frontend/app/components/inputs/ImageUpload.tsx
@@ -1,7 +1,5 @@
-import { useCallback } from "react";
 import { CldUploadWidget } from "next-cloudinary";
 import Image from "next/image";
-import { TbPhotoPlus } from 'react-icons/tb';
 
 declare global {
   var cloudinary: any;
@@ -10,28 +8,19 @@ declare global {
 const uploadPreset = "rkb0drs3";
 
 interface ImageUploadProps {
-  onChange: (value: string[]) => void; // Adjusted to accept an array of strings
-  value: string[]; // Adjusted to be an array
+  onChange: (value: string[]) => void; 
+  value: string[];
 }
 
-const ImageUpload: React.FC<ImageUploadProps> = ({
-  onChange,
-  value
-}) => {
+const ImageUpload: React.FC<ImageUploadProps> = ({ onChange, value }) => {
   const handleUpload = (result: any) => {
-
-    //console.log({result})
     const uploadedUrls = result.info.secure_url;
-    //console.log({uploadedUrls})
-    onChange([...value,uploadedUrls]);
-  }
-
-  //console.log({value},"value")
+    onChange([...value, uploadedUrls]);
+  };
 
   return (
     <CldUploadWidget
       onUpload={handleUpload}
-       
       uploadPreset={uploadPreset}
       options={{
         multiple: true,
@@ -42,35 +31,47 @@ const ImageUpload: React.FC<ImageUploadProps> = ({
           <div
             onClick={() => open?.()}
             className="
-              relative
-              cursor-pointer
-              hover:opacity-70
-              transition
-              border-dashed 
-              border-2 
-              p-20 
-              border-neutral-300
-              flex
-              flex-col
-              justify-center
-              items-center
-              gap-4
-              text-neutral-600
+            relative
+            cursor-pointer
+            transition
+            border-dashed 
+            border-gray-300
+            p-10
+            rounded-lg
+            flex
+            flex-col
+            justify-center
+            items-center
+            gap-4
+            text-gray-600
+            mt-[-2rem]
             "
           >
-            <TbPhotoPlus size={50} />
-            <div className="font-semibold text-lg">
-              Click to upload
-            </div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="w-12 h-12 text-gray-400 group-hover:text-gray-600"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M3 7v4a1 1 0 001 1h3m10 0h3a1 1 0 001-1V7m-7 12V10m0 12v-5m0 5H9m3 0h3"
+              />
+            </svg>
+            <div className="font-semibold text-lg">Click to upload</div>
             {value?.map((imageUrl, index) => (
               <div key={index} className="absolute inset-0 w-full h-full">
-                {imageUrl!==''  &&  <Image
-                  fill
-                  style={{ objectFit: 'cover' }}
-                  src={imageUrl}
-                  alt={`Uploaded image ${index + 1}`}
-                />}
-               
+                {imageUrl !== "" && (
+                  <Image
+                    fill
+                    style={{ objectFit: "cover" }}
+                    src={imageUrl}
+                    alt={`Uploaded image ${index + 1}`}
+                  />
+                )}
               </div>
             ))}
           </div>
