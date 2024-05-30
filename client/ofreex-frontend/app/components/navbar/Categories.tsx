683998528beffaf1d23899ef547d6f9bed81f3df
rupeshshandilya <rupeshkshandilya@gmail.com>
2024-05-30 16:24:15 +0530

    added interface and id prop to categoryBox

--- client/ofreex-frontend/app/components/navbar/Categories.tsx
+++ client/ofreex-frontend/app/components/navbar/Categories.tsx
@@ -1,22 +1,31 @@
-'use client';
+"use client";
 
-import { usePathname, useSearchParams } from 'next/navigation';
+import { usePathname, useSearchParams } from "next/navigation";
 import { SafeCategory } from "@/app/types";
 import CategoryBox from "../CategoryBox";
-import Container from '../Container';
-
+import Container from "../Container";
 
 export let categories: SafeCategory[] | null = [];
 interface CategoryProps {
   categoriesProps: SafeCategory[] | null;
 }
 
+interface SubCategory {
+  id: string;
+  label: string;
+}
+
+interface Category {
+  label: string;
+  subCategories: SubCategory[];
+}
+
 const Categories: React.FC<CategoryProps> = ({ categoriesProps }) => {
-  categories = categoriesProps;
+  let categories = categoriesProps;
   const params = useSearchParams();
-  const category = params?.get('category');
+  const category = params?.get("category");
   const pathname = usePathname();
-  const isMainPage = pathname === '/';
+  const isMainPage = pathname === "/";
 
   if (!isMainPage) {
     return null;
@@ -26,25 +35,24 @@ const Categories: React.FC<CategoryProps> = ({ categoriesProps }) => {
     <Container>
       <div
         className="
-          md:pt-4
-          flex 
-          flex-row 
-          items-center 
-          justify-between
-          overflow-x-auto
+          flex flex-row space-x-4 py-4
         "
       >
-        {categories ? categories.map((item) => (
-          <CategoryBox
-            key={item.label}
-            label={item.label}
-            icon={item.icon || null}
-            selected={category === item.label}
-          />
-        )) : null}
+        {categories
+          ? categories.map((item) => (
+              <CategoryBox
+                id={item.id}
+                key={item.label}
+                label={item.label}
+                icon={item.icon || null}
+                selected={category === item.label}
+                subCategories={item.subCategories}
+              />
+            ))
+          : null}
       </div>
     </Container>
   );
-}
+};
 
 export default Categories;
