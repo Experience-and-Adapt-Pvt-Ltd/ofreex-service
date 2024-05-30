636151e86af3e383fc6e58fa1327dbd1ddf3d5a4
rupeshshandilya <rupeshkshandilya@gmail.com>
2024-05-30 16:32:34 +0530

    init

--- client/ofreex-frontend/tailwind.config.js
+++ client/ofreex-frontend/tailwind.config.js
@@ -7,7 +7,18 @@ module.exports = {
     "components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: {
+          100: '#f7fafc',
+          200: '#edf2f7',
+        },
+        black: '#000',
+      },
+      boxShadow: {
+        lg: '0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -4px rgba(0, 0, 0, 0.1)',
+      }
+    }
   },
   plugins: [],
 };
