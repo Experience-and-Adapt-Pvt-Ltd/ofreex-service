382672d35efd0b191c46d0aa424485dbc6588da0
rupeshshandilya <rupeshkshandilya@gmail.com>
2024-06-24 09:09:39 +0530

    handled onMouseEnter and onMouseLeave for subCategory

--- client/ofreex-frontend/app/components/CategoryBox.tsx
+++ client/ofreex-frontend/app/components/CategoryBox.tsx
@@ -13,8 +13,8 @@ interface CategoryBoxProps {
     id: string;
     label: string;
   }[];
-  onMouseEnter: any;
-  onMouseLeave: any;
+  onMouseEnter?: any;
+  onMouseLeave?: any;
 }
 
 const CategoryBox: React.FC<CategoryBoxProps> = ({
@@ -38,53 +38,7 @@ const CategoryBox: React.FC<CategoryBoxProps> = ({
     router.push(newUrl);
   };
 
-  // const handleClick = useCallback(() => {
-  //   let currentQuery = {};
-
-  //   if (params) {
-  //     currentQuery = qs.parse(params.toString());
-  //   }
-
-  //   const updatedQuery: any = {
-  //     ...currentQuery,
-  //     category: label,
-  //   };
-
-  //   if (params?.get("category") === label) {
-  //     delete updatedQuery.category;
-  //   }
-
-  //   const url = qs.stringifyUrl(
-  //     {
-  //       url: "/",
-  //       query: updatedQuery,
-  //     },
-  //     { skipNull: true }
-  //   );
-
-  //   router.push(url);
-  // }, [label, router, params]);
   return (
-    // <div onClick={toggleDropdown} className="relative cursor-pointer flex flex-col items-center py-2 px-4">
-    //   <img src={categoryIcon} alt="category icon" className="h-5 mb-2"/>
-    //   <div className="font-medium text-sm">{label}</div>
-    //   {isOpen && (
-    //     <div
-    //       className="absolute rounded-sm z-10 bg-white dark:bg-black shadow-md w-[40vw] md:w-3/4 right-0 top-14 md:top-12 text-sm"
-    //       onMouseLeave={() => setIsOpen(false)} // Optionally close the dropdown on mouse leave
-    //     >
-    //       {subCategories.map((subCategory) => (
-    //         <div
-    //           key={subCategory.id}
-    //           onClick={(event) => handleSubCategorySelect(event, subCategory.id)}
-    //           className="flex flex-col cursor-pointer my-4 px-3 pt-1"
-    //         >
-    //           {subCategory.label}
-    //         </div>
-    //       ))}
-    //     </div>
-    //   )}
-    // </div>
     <div
     onMouseEnter={onMouseEnter}
     onMouseLeave={onMouseLeave}
@@ -98,10 +52,7 @@ const CategoryBox: React.FC<CategoryBoxProps> = ({
       <img src={categoryIcon} alt={`${label} icon`} className="ml-2 -mr-1 h-5 w-5" />
     </div>
     {selected && (
-      <div
-        className="origin-top-right absolute mt-1 w-32 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
-        role="menu"
-      >
+     <div className="origin-top-right absolute mt-1 w-32 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu">
         {subCategories.map(subCategory => (
           <div
             key={subCategory.id}
