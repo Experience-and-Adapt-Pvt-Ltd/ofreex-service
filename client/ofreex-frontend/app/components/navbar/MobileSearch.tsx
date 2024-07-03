eca7cb52c7580110272c1d4b5f2f49a89f0df7a5
Rupesh Shandilya <rupeshshandilya@Rupeshs-MacBook-Pro.local>
2024-07-04 02:07:22 +0530

    fixed blank search bug

--- client/ofreex-frontend/app/components/navbar/MobileSearch.tsx
+++ client/ofreex-frontend/app/components/navbar/MobileSearch.tsx
@@ -9,24 +9,16 @@ const MobileSearch = () => {
 
   const onSearch = (event: React.FormEvent) => {
     event.preventDefault();
+    //prevent from blank search
+    if(searchQuery.trim() === ""){
+      return ;
+    }
     const encodedSearchQuery = encodeURI(searchQuery);
     router.push(`/search?q=${encodedSearchQuery}`);
     console.log(encodedSearchQuery);
   };
 
   return (
-    // <form className="mt-4 w-full relative left-1 right-1" onSubmit={onSearch}>
-    //   <input
-    //     type="text"
-    //     placeholder="What are you looking for?"
-    //     value={searchQuery}
-    //     onChange={(event) => setSearchQuery(event.target.value)}
-    //     className="px-6 py-3 text-black border border-zinc-800 rounded-xl w-full"
-    //   />
-    //   <button type="submit" className="relative  left-[10rem]">
-    //     <IoSearch className="h-8" />
-    //   </button>
-    // </form>
     <form
     className="flex items-center bg-white mt-4 dark:bg-zinc-800 rounded-full p-2"
     onSubmit={onSearch}>
